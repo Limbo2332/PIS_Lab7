4d8a5de2e50bd89d98f4a27347b308b6f887bc9b
Limbo2332 <93576084+Limbo2332@users.noreply.github.com>
2023-05-14 18:49:51 +0300

    Add project files.

---
+++ PIS_Lab7/Program.cs
@@ -0,0 +1,33 @@
+using PIS_Lab7.Hubs;
+
+namespace PIS_Lab7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddSignalR();
+
+            var app = builder.Build();
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapHub<ChatHub>("/chatHub");
+
+
+            app.Run();
+        }
+    }
+}
